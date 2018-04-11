4bd1fd488c24d27899d47140c9ca2f985da9b9bd
issacsanchez <issac.sanchez.dev@gmail.com>
2018-04-11 00:39:45 -0500

    return json embeds

--- facebook/ajax/facebook_feed_GET.php
+++ facebook/ajax/facebook_feed_GET.php
@@ -7,13 +7,20 @@ session_start();
 $config = require '/var/www/html/monosmash/facebook/config_facebook.php';
 $userid = TESTLOGIN::isLoggedIn();
 
-		function hasFacebook() {
-			global $userid;
+		function hasFacebook($userid) {
 			if(DB::query('SELECT user_id FROM facebook WHERE user_id=:userid', array(':userid' => $userid))) {
 				return true;
 			} else {
 				return false;
 			}
+		}
+		function LoadFacebookCredentials($userid) {
+			$sql_creds = DB::query('SELECT * FROM facebook WHERE user_id=:userid', array(':userid' => $userid));
+			$fb_at = $sql_creds[0]['fb_access_token'];
+			$creds = array(
+				"fb_at" => $fb_at
+			);
+			return $creds;
 		}
 		function GetFacebookObject($config) {
 			// connect with user token
@@ -27,20 +34,19 @@ $userid = TESTLOGIN::isLoggedIn();
 		function GetPostsURL($facebook, $creds) {
 			try {
 				$posts = [];
-				$html = [];
 				$response = $facebook->get('/me/likes?fields=id,name', $creds['fb_at']);
 				$graphEdge = $response->getGraphEdge();
 				foreach($graphEdge as $graphNode) {
 					$pageposts = $facebook->get('/' . $graphNode['id'] . '/posts?limit=1&fields=permalink_url,created_time,description', $creds['fb_at']);
 					$edge = $pageposts->getGraphEdge();
 					foreach($edge as $node) {
-						$posts[] = $node['permalink_url'];
+						$posts[] = array('created_time'=>$node['created_time'], 'provider'=>'facebook', 'post_url'=>$node['permalink_url']);
 					}
 				}
 				return $posts;
 
 			} catch(Facebook\Exceptions\FacebookResponseException $e) {
-				echo 'Graph returned an error: ' . $e->getMessage();
+				echo 'Graph returned an error: at GetPostsURL top level' . $e->getMessage();
 				exit;
 			} catch(Facebook\Exceptions\FacebookSDKException $e) {
 				echo 'Facebook SDK returned an error: ' . $e->getMessage();
@@ -51,46 +57,26 @@ $userid = TESTLOGIN::isLoggedIn();
 			$embeds = [];
 			$oembed_endpoint = 'https://www.facebook.com/plugins/post/oembed';
 			foreach($posts as $post) {
-				$json_url = $oembed_endpoint . '.json/?url=' . rawurlencode($post) . '&maxwidth=550&omitscript=1';
+				$json_url = $oembed_endpoint . '.json/?url=' . rawurlencode($post['post_url']) . '&maxwidth=550&omitscript=1';
 				$curl = curl_init($json_url);
 				curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 				curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
 				curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36');
 				$result = curl_exec($curl);
 				$jsonr = json_decode($result, true);
-				$embeds[] = $jsonr['html'];
+				$embeds[] = array('created_time'=>$post['created_time'], 'provider'=>$post['provider'], 'html'=>$jsonr['html']);
 				curl_close($curl);
 			}
 			return $embeds;
 		}
-		function BuildHTML($embeds) {
-			$fb_script = '<script>(function (d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s);js.id = id;js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11";fjs.parentNode.insertBefore(js, fjs);}(document, "script", "facebook-jssdk"));</script>';
-			$feed_ar_to_str = '';
-			foreach($embeds as $html) {
-				$feed_ar_to_str .= $html;
-			}
-			$final_feed = $fb_script . $feed_ar_to_str;
-			return $final_feed;
-		}
-		function LoadFacebookCredentials() {
-			global $userid;
-			$sql_creds = DB::query('SELECT * FROM facebook WHERE user_id=:userid', array(':userid' => $userid));
-			$fb_at = $sql_creds[0]['fb_access_token'];
-			$creds = array(
-				"fb_at" => $fb_at
-			);
-			return $creds;
-		}
 
-		if(hasFacebook()) {
-			$creds = LoadFacebookCredentials();
+		if(hasFacebook($userid)) {
+			$creds = LoadFacebookCredentials($userid);
 			$facebook = GetFacebookObject($config);
-			$postsURL = GetPostsURL($facebook, $creds);
-			$embeds = GetEmbeds($postsURL);
-			$final_html = BuildHTML($embeds);
-			//header('Content-Type: application/json');
-			//echo json_encode($embeds);
-			echo $final_html;
+			$posts = GetPostsURL($facebook, $creds);
+			$embeds = GetEmbeds($posts);
+			header('Content-Type: application/json');
+			echo json_encode($embeds);
 		} else {
 			die('USER HAS NO facebook');
 		}
